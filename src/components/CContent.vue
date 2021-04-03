3f77dc8735dcee1a1371693f15fc9135f4ca8237
congnv <congnv3030@gmail.com>
2021-04-03 18:14:36 +0700

    Update small UI

--- src/components/CContent.vue
+++ src/components/CContent.vue
@@ -2,12 +2,12 @@
   <div class="text-center mt-22">
     <h4 class="mb-3">Nguyễn Văn Công</h4>
     <div class="mb-5 text-center">
-      <div class="rounded-full inline-block px-4 py-1 bg-gray-200 transition text-sm">
+      <div class="rounded-full inline-block px-4 py-1 bg-gray-200 text-gray-600 transition text-sm">
         Web Developer
       </div>
     </div>
     <p class="text-black mb-5">
-      I am a student at Ho Chi Minh City University of Education. My main job is <span class="font-semibold">web</span> development.
+      I'm a student at Ho Chi Minh City University of Education. My main job is <span class="font-semibold">web</span> development.
     </p>
     <div class="text-center">
       <a href="#" class="appearance-none py-2 text-base px-6 sm:px-8 md:px-10 rounded-full text-white font-semibold text-center inline-block bg-blue-500 select-none transition-all hover:bg-blue-600 focus:outline-0 focus:bg-blue-600 focus:ring focus:ring-blue-300">
