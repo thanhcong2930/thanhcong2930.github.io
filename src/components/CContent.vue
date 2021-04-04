358c477de94cb0fa8c53cf2ec842bc7a4e602d72
congnv <congnv3030@gmail.com>
2021-04-04 16:36:54 +0700

    Update content

--- src/components/CContent.vue
+++ src/components/CContent.vue
@@ -7,7 +7,7 @@
       </div>
     </div>
     <p class="text-black dark:text-white mb-5">
-      I'm a student at Ho Chi Minh City University of Education. My main job is <span class="font-semibold">web</span> development.
+      I'm a student at HCM City University of Education. My main job is <span class="font-semibold">web</span> development.
     </p>
     <div class="text-center">
       <a href="#" class="appearance-none py-2 text-base px-6 sm:px-8 md:px-10 rounded-full text-white font-semibold text-center inline-block bg-blue-500 select-none transition-all hover:bg-blue-600 focus:outline-0 focus:bg-blue-600 focus:ring focus:ring-blue-300">
